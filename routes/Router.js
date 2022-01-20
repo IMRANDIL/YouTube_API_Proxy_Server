40f364a1aa34a4b2cf962ed8ad5a6bf13e96a9d0
IMRANDIL <aliimranadil2@gmail.com>
2022-01-20 13:38:22 +0530

    yh

--- routes/Router.js
+++ routes/Router.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch')
 
 router.get('/videos', async (req, res) => {
     try {
-        const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PL8p2I9GklV45ihqIep4n3_VijItAkcibN&key=${process.env.KEY}`
+        const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PL8p2I9GklV45ihqIep4n3_VijItAkcibN&maxResults=50&key=${process.env.KEY}`
         const data = await fetch(url);
         const reply = await data.json()
         res.json(reply.items)
