fd678cf46a81670923dea1a0948e8e75e150adb8
IMRANDIL <aliimranadil2@gmail.com>
2022-01-20 10:59:16 +0530

    yh

--- routes/Router.js
+++ routes/Router.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
 
+const fetch = require('node-fetch')
 
 //get..
 
-router.get('/videos', (req, res) => {
-    res.json([]);
+router.get('/videos', async (req, res) => {
+    try {
+        const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UULNgu_OupwoeESgtab33CCw&key=${process.env.KEY}`
+        const data = await fetch(url);
+        const reply = await data.json()
+        res.json(reply.items)
+    } catch (error) {
+        console.log(error);
+    }
+
 })
 
 
